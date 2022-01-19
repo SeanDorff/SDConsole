8d297e52a02b08834b832f9d843deaeb4baccde4
SeanDorff <seandorff@googlemail.com>
2022-01-19 19:48:03 +0100

    Enhanced code documentation

--- SDConsole/SDConsole.cs
+++ SDConsole/SDConsole.cs
@@ -30,44 +30,149 @@ namespace SDConNS
         /// <summary>
         /// Returns the lock object for convenient synchronization.
         /// </summary>
-        /// <returns></returns>
+        /// <returns><see cref="object"/> cursor state lock</returns>
         public static object GetCursorStateLock() => cursorStateLock;
 
+        /// <summary>
+        /// See <see cref="Console.Write(bool)"/>
+        /// </summary>
         public static void Write(bool value) => WriteBufferWriter.Enqueue(new WriteBufferContainer(false, value));
+        /// <summary>
+        /// See <see cref="Console.Write(char)"/>
+        /// </summary>
         public static void Write(char value) => WriteBufferWriter.Enqueue(new WriteBufferContainer(false, value));
+        /// <summary>
+        /// See <see cref="Console.Write(char[]?)"/>
+        /// </summary>
         public static void Write(char[]? buffer) => WriteBufferWriter.Enqueue(new WriteBufferContainer(false, buffer));
+        /// <summary>
+        /// See <see cref="Console.Write(char[], int, int)"/>
+        /// </summary>
         public static void Write(char[] buffer, int index, int count) => WriteBufferWriter.Enqueue(new WriteBufferContainer(false, buffer, index, count));
+        /// <summary>
+        /// See <see cref="Console.Write(decimal)"/>
+        /// </summary>
         public static void Write(decimal value) => WriteBufferWriter.Enqueue(new WriteBufferContainer(false, value));
+        /// <summary>
+        /// See <see cref="Console.Write(double)"/>
+        /// </summary>
         public static void Write(double value) => WriteBufferWriter.Enqueue(new WriteBufferContainer(false, value));
+        /// <summary>
+        /// See <see cref="Console.Write(int)"/>
+        /// </summary>
         public static void Write(int value) => WriteBufferWriter.Enqueue(new WriteBufferContainer(false, value));
+        /// <summary>
+        /// See <see cref="Console.Write(long)"/>
+        /// </summary>
         public static void Write(long value) => WriteBufferWriter.Enqueue(new WriteBufferContainer(false, value));
+        /// <summary>
+        /// See <see cref="Console.Write(object?)"/>
+        /// </summary>
         public static void Write(object? value) => WriteBufferWriter.Enqueue(new WriteBufferContainer(false, value));
+        /// <summary>
+        /// See <see cref="Console.Write(float)"/>
+        /// </summary>
         public static void Write(float value) => WriteBufferWriter.Enqueue(new WriteBufferContainer(false, value));
+        /// <summary>
+        /// See <see cref="Console.Write(string?)"/>
+        /// </summary>
         public static void Write(string? value) => WriteBufferWriter.Enqueue(new WriteBufferContainer(false, value));
+        /// <summary>
+        /// See <see cref="Console.Write(string, object?)"/>
+        /// </summary>
         public static void Write(string format, object? arg0) => WriteBufferWriter.Enqueue(new WriteBufferContainer(false, format, arg0));
+        /// <summary>
+        /// See <see cref="Console.Write(string, object?, object?)"/>
+        /// </summary>
         public static void Write(string format, object? arg0, object? arg1) => WriteBufferWriter.Enqueue(new WriteBufferContainer(false, format, arg0, arg1));
+        /// <summary>
+        /// See <see cref="Console.Write(string, object?, object?, object?)"/>
+        /// </summary>
         public static void Write(string format, object? arg0, object? arg1, object? arg2) => WriteBufferWriter.Enqueue(new WriteBufferContainer(false, format, arg0, arg1, arg2));
+        /// <summary>
+        /// See <see cref="Console.Write(string, object?[]?)"/>
+        /// </summary>
         public static void Write(string format, params object?[]? arg) => WriteBufferWriter.Enqueue(new WriteBufferContainer(false, format, arg));
+        /// <summary>
+        /// See <see cref="Console.Write(uint)"/>
+        /// </summary>
         public static void Write(uint value) => WriteBufferWriter.Enqueue(new WriteBufferContainer(false, value));
+        /// <summary>
+        /// See <see cref="Console.Write(ulong)"/>
+        /// </summary>
         public static void Write(ulong value) => WriteBufferWriter.Enqueue(new WriteBufferContainer(false, value));
-        public static void WriteLine() => WriteBufferWriter.Enqueue(new WriteBufferContainer(true, ""));
-        public static void WriteLine(bool value) => WriteBufferWriter.Enqueue(new WriteBufferContainer(true, value));
-        public static void WriteLine(char value) => WriteBufferWriter.Enqueue(new WriteBufferContainer(true, value));
-        public static void WriteLine(char[]? buffer) => WriteBufferWriter.Enqueue(new WriteBufferContainer(true, buffer));
-        public static void WriteLine(char[] buffer, int index, int count) => WriteBufferWriter.Enqueue(new WriteBufferContainer(true, buffer, index, count));
-        public static void WriteLine(decimal value) => WriteBufferWriter.Enqueue(new WriteBufferContainer(true, value));
-        public static void WriteLine(double value) => WriteBufferWriter.Enqueue(new WriteBufferContainer(true, value));
-        public static void WriteLine(int value) => WriteBufferWriter.Enqueue(new WriteBufferContainer(true, value));
-        public static void WriteLine(long value) => WriteBufferWriter.Enqueue(new WriteBufferContainer(true, value));
-        public static void WriteLine(object? value) => WriteBufferWriter.Enqueue(new WriteBufferContainer(true, value));
-        public static void WriteLine(float value) => WriteBufferWriter.Enqueue(new WriteBufferContainer(true, value));
-        public static void WriteLine(string? value) => WriteBufferWriter.Enqueue(new WriteBufferContainer(true, value));
-        public static void WriteLine(string format, object? arg0) => WriteBufferWriter.Enqueue(new WriteBufferContainer(true, format, arg0));
-        public static void WriteLine(string format, object? arg0, object? arg1) => WriteBufferWriter.Enqueue(new WriteBufferContainer(true, format, arg0, arg1));
-        public static void WriteLine(string format, object? arg0, object? arg1, object? arg2) => WriteBufferWriter.Enqueue(new WriteBufferContainer(true, format, arg0, arg1, arg2));
-        public static void WriteLine(string format, params object?[]? arg) => WriteBufferWriter.Enqueue(new WriteBufferContainer(true, format, arg));
-        public static void WriteLine(uint value) => WriteBufferWriter.Enqueue(new WriteBufferContainer(true, value));
-        public static void WriteLine(ulong value) => WriteBufferWriter.Enqueue(new WriteBufferContainer(true, value));
+        /// <summary>
+        /// See <see cref="Console.WriteLine()"/>
+        /// </summary>
+        public static void WriteLine() { WriteBufferWriter.Enqueue(new WriteBufferContainer(true, "")); LineFeed(); }
+        /// <summary>
+        /// See <see cref="Console.WriteLine(bool)"/>
+        /// </summary>
+        public static void WriteLine(bool value) { WriteBufferWriter.Enqueue(new WriteBufferContainer(true, value)); LineFeed(); }
+        /// <summary>
+        /// See <see cref="Console.WriteLine(char)"/>
+        /// </summary>
+        public static void WriteLine(char value) { WriteBufferWriter.Enqueue(new WriteBufferContainer(true, value)); LineFeed(); }
+        /// <summary>
+        /// See <see cref="Console.WriteLine(char[]?)"/>
+        /// </summary>
+        public static void WriteLine(char[]? buffer) { WriteBufferWriter.Enqueue(new WriteBufferContainer(true, buffer)); LineFeed(); }
+        /// <summary>
+        /// See <see cref="Console.WriteLine(char[], int, int)"/>
+        /// </summary>
+        public static void WriteLine(char[] buffer, int index, int count) { WriteBufferWriter.Enqueue(new WriteBufferContainer(true, buffer, index, count)); LineFeed(); }
+        /// <summary>
+        /// See <see cref="Console.WriteLine(decimal)"/>
+        /// </summary>
+        public static void WriteLine(decimal value) { WriteBufferWriter.Enqueue(new WriteBufferContainer(true, value)); LineFeed(); }
+        /// <summary>
+        /// See <see cref="Console.WriteLine(double)"/>
+        /// </summary>
+        public static void WriteLine(double value) { WriteBufferWriter.Enqueue(new WriteBufferContainer(true, value)); LineFeed(); }
+        /// <summary>
+        /// See <see cref="Console.WriteLine(int)"
+        /// </summary>
+        public static void WriteLine(int value) { WriteBufferWriter.Enqueue(new WriteBufferContainer(true, value)); LineFeed(); }
+        /// <summary>
+        /// See <see cref="Console.WriteLine(long)"/>
+        /// </summary>
+        public static void WriteLine(long value) { WriteBufferWriter.Enqueue(new WriteBufferContainer(true, value)); LineFeed(); }
+        /// <summary>
+        /// See <see cref="Console.WriteLine(object?)"/>
+        /// </summary>
+        public static void WriteLine(object? value) { WriteBufferWriter.Enqueue(new WriteBufferContainer(true, value)); LineFeed(); }
+        /// <summary>
+        /// See <see cref="Console.WriteLine(float)"/>
+        /// </summary>
+        public static void WriteLine(float value) { WriteBufferWriter.Enqueue(new WriteBufferContainer(true, value)); LineFeed(); }
+        /// <summary>
+        /// See <see cref="Console.WriteLine(string?)"/>
+        /// </summary>
+        public static void WriteLine(string? value) { WriteBufferWriter.Enqueue(new WriteBufferContainer(true, value)); LineFeed(); }
+        /// <summary>
+        /// See <see cref="Console.WriteLine(string, object?)"/>
+        /// </summary>
+        public static void WriteLine(string format, object? arg0) { WriteBufferWriter.Enqueue(new WriteBufferContainer(true, format, arg0)); LineFeed(); }
+        /// <summary>
+        /// See <see cref="Console.WriteLine(string, object?, object?)"/>
+        /// </summary>
+        public static void WriteLine(string format, object? arg0, object? arg1) { WriteBufferWriter.Enqueue(new WriteBufferContainer(true, format, arg0, arg1)); LineFeed(); }
+        /// <summary>
+        /// See <see cref="Console.WriteLine(string, object?, object?, object?)"/>
+        /// </summary>
+        public static void WriteLine(string format, object? arg0, object? arg1, object? arg2) { WriteBufferWriter.Enqueue(new WriteBufferContainer(true, format, arg0, arg1, arg2)); LineFeed(); }
+        /// <summary>
+        /// See <see cref="Console.WriteLine(string, object?[]?)"/>
+        /// </summary>
+        public static void WriteLine(string format, params object?[]? arg) { WriteBufferWriter.Enqueue(new WriteBufferContainer(true, format, arg)); LineFeed(); }
+        /// <summary>
+        /// See <see cref="Console.WriteLine(uint)"/>
+        /// </summary>
+        public static void WriteLine(uint value) { WriteBufferWriter.Enqueue(new WriteBufferContainer(true, value)); LineFeed(); }
+        /// <summary>
+        /// See <see cref="Console.WriteLine(ulong)"/>
+        /// </summary>
+        public static void WriteLine(ulong value) { WriteBufferWriter.Enqueue(new WriteBufferContainer(true, value)); LineFeed(); }
 
         /// <summary>
         /// Set a custom thread sleep time.
@@ -79,5 +184,7 @@ namespace SDConNS
         /// Reset the thread sleep time to the default.
         /// </summary>
         public static void ResetThreadSleepTime() => WriteBufferWriter.ResetThreadSleepTime();
+
+        private static void LineFeed() => Console.SetCursorPosition(0, Console.GetCursorPosition().Top + 1);
     }
 }
